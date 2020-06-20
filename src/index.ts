30472d33c65c262c27904c85082089451fe969a4
Alkihis <alkihis@gmail.com>
2020-06-20 17:57:39 +0200

    Added outside proposal methods

--- src/index.ts
+++ src/index.ts
@@ -43,16 +43,50 @@ interface Iterator<T, TReturn = any, TNext = undefined> {
   every(callback: (value: T) => boolean) : boolean;
   /** Return `true` if one value of iterator validate {callback}. */
   some(callback: (value: T) => boolean) : boolean;
-  /** Join every item of iterator with {separator}, and return the built string. */
-  join(separator: string) : string;
   /** Consume iterator and collapse values inside an array. */
   toArray(max_count?: number) : T[];
-  /** Count number of values within iterable. It consumes it. */
-  count() : number;
   /** Accumulate each item inside **acc** for each value **value**. */
   reduce<V>(reducer: (acc: V, value: T) => V, initial_value?: V) : V;
   /** Iterate over each value of iterator by calling **callback** for each value. */
   forEach(callback: (value: T) => any) : void;
+
+  /** Join every item of iterator with {separator}, and return the built string. */
+  join(separator: string) : string;
+  /** Count number of values within iterable. It consumes it. */
+  count() : number;
+  /** Chain iterables together, in provided order. */
+  chain<I>(...iterables: IterableIterator<I>[]) : Iterator<T | I, TReturn, TNext>;
+  /** Iterate through multiple iterators together */
+  zip<O>(...others: IterableIterator<O>[]) : Iterator<(T | O)[], TReturn, TNext>;
+  /** 
+   * It will call this closure on each element of the iterator, and ignore elements until it returns false. 
+   * After false is returned, dropWhile()'s job is over, and the rest of the elements are yielded. 
+   */
+  dropWhile(callback: (value: T) => boolean) : Iterator<T, TReturn, TNext>;
+  /** 
+   * It will call this closure on each element of the iterator, and yield elements until it returns false. 
+   * After false is returned, takeWhile()'s job is over, and the rest of the elements are ignored. 
+   */
+  takeWhile(callback: (value: T) => boolean) : Iterator<T, TReturn, TNext>;
+  /** 
+   * Yield items until one item is `null` or `undefined`.
+   * 
+   * Shortcut for `.takeWhile(e => e !== undefined && e !== null)`.
+   */
+  fuse() : Iterator<T, TReturn, TNext>;
+  /** 
+   * Consume iterator to create two partitions : first is filled when `callback(item)` is `true`, 
+   * second when `callback(item)` is `false`. 
+   */
+  partition(callback: (value: T) => boolean) : [T[], T[]];
+  /** Find index in iterator for the first item where {callback} returns `true` (consume iterator). */
+  findIndex(callback: (value: T) => boolean) : number;
+  /** Return the max element in iterator (consume it). */
+  max() : T;
+  /** Return the max element in iterator (consume it). */
+  min() : T;
+  /** Instead of stopping when iterator is consumed, the iterator will instead start again, from the beginning. Forever. */
+  cycle() : Iterator<T, TReturn, TNext>;
 }
 
 interface AsyncIterator<T, TReturn = any, TNext = undefined> {
@@ -74,16 +108,50 @@ interface AsyncIterator<T, TReturn = any, TNext = undefined> {
   every(callback: (value: T) => boolean) : Promise<boolean>;
   /** Return `true` if one value of iterator validate {callback}. */
   some(callback: (value: T) => boolean) : Promise<boolean>;
-  /** Join every item of iterator with {separator}, and return the built string. */
-  join(separator: string) : Promise<string>;
   /** Consume iterator and collapse values inside an array. */
   toArray(max_count?: number) : Promise<T[]>;
-  /** Count number of values within iterable. It consumes it. */
-  count() : Promise<number>;
   /** Accumulate each item inside **acc** for each value **value**. */
   reduce<V>(reducer: (acc: V, value: T) => V, initial_value?: V) : Promise<V>;
   /** Iterate over each value of iterator by calling **callback** for each value. */
   forEach(callback: (value: T) => any) : Promise<void>;
+
+  /** Join every item of iterator with {separator}, and return the built string. */
+  join(separator: string) : Promise<string>;
+  /** Count number of values within iterable. It consumes it. */
+  count() : Promise<number>;
+  /** Chain iterables together, in provided order. */
+  chain<I>(...iterables: AsyncIterableIterator<I>[]) : AsyncIterator<T | I, TReturn, TNext>;
+  /** Iterate through multiple iterators together */
+  zip<O>(...others: AsyncIterableIterator<O>[]) : AsyncIterator<(T | O)[], TReturn, TNext>;
+  /** 
+   * It will call this closure on each element of the iterator, and ignore elements until it returns false. 
+   * After false is returned, dropWhile()'s job is over, and the rest of the elements are yielded. 
+   */
+  dropWhile(callback: (value: T) => boolean) : AsyncIterator<T, TReturn, TNext>;
+  /** 
+   * It will call this closure on each element of the iterator, and yield elements until it returns false. 
+   * After false is returned, takeWhile()'s job is over, and the rest of the elements are ignored. 
+   */
+  takeWhile(callback: (value: T) => boolean) : AsyncIterator<T, TReturn, TNext>;
+  /** 
+   * Yield items until one item is `null` or `undefined`.
+   * 
+   * Shortcut for `.takeWhile(e => e !== undefined && e !== null)`.
+   */
+  fuse() : AsyncIterator<T, TReturn, TNext>;
+  /** 
+   * Consume iterator to create two partitions : first is filled when `callback(item)` is `true`, 
+   * second when `callback(item)` is `false`. 
+   */
+  partition(callback: (value: T) => boolean) : Promise<[T[], T[]]>;
+  /** Find index in iterator for the first item where {callback} returns `true` (consume iterator). */
+  findIndex(callback: (value: T) => boolean) : Promise<number>;
+  /** Return the max element in iterator (consume it). */
+  max() : Promise<T>;
+  /** Return the max element in iterator (consume it). */
+  min() : Promise<T>;
+  /** Instead of stopping when iterator is consumed, the iterator will instead start again, from the beginning. Forever. */
+  cycle() : AsyncIterator<T, TReturn, TNext>;
 }
 
 
@@ -171,21 +239,6 @@ interface AsyncIterator<T, TReturn = any, TNext = undefined> {
         return false;
       }
     },
-    join: {
-      value(string: string) {
-        let final = '';
-        let first = true;
-
-        for (const value of this) {
-          if (first) 
-            first = false;
-          else 
-            final += string + value;
-        }
-
-        return final;
-      }
-    },
     toArray: {
       value(max_count = Infinity) {
         const values = [];
@@ -202,16 +255,6 @@ interface AsyncIterator<T, TReturn = any, TNext = undefined> {
 
         return values;
       }
-    },
-    count: {
-      value() {
-        let count = 0;
-        
-        for (const _ of this)
-          count++;
-
-        return count;
-      },
     },
     take: {
       *value(limit: number) {
@@ -299,6 +342,137 @@ interface AsyncIterator<T, TReturn = any, TNext = undefined> {
     [Symbol.toStringTag]: {
       value: 'IteratorPrototype'
     },
+
+    /* OUTSIDE PROPOSAL */
+    count: {
+      value() {
+        let count = 0;
+        
+        for (const _ of this)
+          count++;
+
+        return count;
+      },
+    },
+    join: {
+      value(string: string) {
+        let final = '';
+        let first = true;
+
+        for (const value of this) {
+          if (first) 
+            first = false;
+          else 
+            final += string + value;
+        }
+
+        return final;
+      }
+    },
+    chain: {
+      *value<I>(...iterables: IterableIterator<I>[]) {
+        yield* this;
+
+        for (const it of iterables) {
+          yield* it;
+        }
+      }
+    },
+    zip: {
+      *value<T, O>(...others: IterableIterator<O>[]) : Iterator<(T | O)[]> {
+        const it_array = [self, ...others].map((e: any) => e[Symbol.iterator]() as Iterator<T | O>);
+        let values = it_array.map(e => e.next());
+
+        while (values.every(e => !e.done)) {
+          yield values.map(e => e.value);
+          values = it_array.map(e => e.next());
+        }
+      },
+    },
+    takeWhile: {
+      *value<T>(callback: (value: T) => boolean) {
+        for (const value of this) {
+          if (callback(value)) 
+            yield value;
+          else
+            break;
+        }
+      }
+    },
+    dropWhile: {
+      *value<T>(callback: (value: T) => boolean) {
+        let finished = false;
+
+        for (const value of this) {
+          if (!finished && callback(value)) 
+            continue;
+          
+          finished = true;
+          yield value;
+        }
+      }
+    },
+    fuse: {
+      value() {
+        return this.takeWhile((e: any) => e !== undefined && e !== null);
+      }
+    },
+    partition: {
+      value<T>(callback: (value: T) => boolean) {
+        const partition1 = [], partition2 = [];
+
+        for (const value of this) {
+          if (callback(value)) 
+            partition1.push(value);
+          else
+            partition2.push(value);
+        }
+
+        return [partition1, partition2];
+      },
+    },
+    findIndex: {
+      value<T>(callback: (value: T) => boolean) {
+        for (const [index, value] of this.asIndexedPairs()) {
+          if (callback(value))
+            return index;
+        }
+
+        return -1;
+      } 
+    },
+    max: {
+      value() {
+        return this.reduce((acc: number, val: number) => {
+          if (acc > val)
+            return acc;
+          return val;
+        });
+      },
+    },
+    min: {
+      value() {
+        return this.reduce((acc: number, val: number) => {
+          if (acc < val)
+            return acc;
+          return val;
+        });
+      },
+    },
+    cycle: {
+      *value() {
+        const values = [];
+        
+        for (const value of this) {
+          values.push(value);
+          yield value;
+        }
+
+        while (true) {
+          yield* values;
+        }
+      },
+    },
   });
 
   /// Polyfill for AsyncIterator
@@ -357,21 +531,6 @@ interface AsyncIterator<T, TReturn = any, TNext = undefined> {
         return false;
       }
     },
-    join: {
-      async value(string: string) {
-        let final = '';
-        let first = true;
-
-        for await (const value of this) {
-          if (first) 
-            first = false;
-          else 
-            final += string + value;
-        }
-
-        return final;
-      }
-    },
     toArray: {
       async value(max_count = Infinity) {
         const values = [];
@@ -388,16 +547,6 @@ interface AsyncIterator<T, TReturn = any, TNext = undefined> {
 
         return values;
       }
-    },
-    count: {
-      async value() {
-        let count = 0;
-        
-        for await (const _ of this)
-          count++;
-
-        return count;
-      },
     },
     take: {
       async *value(limit: number) {
@@ -484,6 +633,139 @@ interface AsyncIterator<T, TReturn = any, TNext = undefined> {
     },
     [Symbol.toStringTag]: {
       value: 'AsyncIteratorPrototype'
+    },
+
+    /* OUTSIDE PROPOSAL */
+    join: {
+      async value(string: string) {
+        let final = '';
+        let first = true;
+
+        for await (const value of this) {
+          if (first) 
+            first = false;
+          else 
+            final += string + value;
+        }
+
+        return final;
+      }
+    },
+    count: {
+      async value() {
+        let count = 0;
+        
+        for await (const _ of this)
+          count++;
+
+        return count;
+      },
+    },
+    chain: {
+      async *value<I>(...iterables: AsyncIterableIterator<I>[]) {
+        yield* this;
+
+        for (const it of iterables) {
+          yield* it;
+        }
+      }
+    },
+    zip: {
+      async *value<T, O>(...others: AsyncIterableIterator<O>[]) : AsyncIterator<(T | O)[]> {
+        const it_array = [self, ...others].map((e: any) => e[Symbol.asyncIterator]() as AsyncIterator<T | O>);
+        let values = await Promise.all(it_array.map(e => e.next()));
+
+        while (values.every(e => !e.done)) {
+          yield values.map(e => e.value);
+          values = await Promise.all(it_array.map(e => e.next()));
+        }
+      },
+    },
+    takeWhile: {
+      async *value<T>(callback: (value: T) => boolean) {
+        for await (const value of this) {
+          if (callback(value)) 
+            yield value;
+          else
+            break;
+        }
+      }
+    },
+    dropWhile: {
+      async *value<T>(callback: (value: T) => boolean) {
+        let finished = false;
+
+        for await (const value of this) {
+          if (!finished && callback(value)) 
+            continue;
+          
+          finished = true;
+          yield value;
+        }
+      }
+    },
+    fuse: {
+      value() {
+        return this.takeWhile((e: any) => e !== undefined && e !== null);
+      }
+    },
+    partition: {
+      async value<T>(callback: (value: T) => boolean) {
+        const partition1 = [], partition2 = [];
+
+        for await (const value of this) {
+          if (callback(value)) 
+            partition1.push(value);
+          else
+            partition2.push(value);
+        }
+
+        return [partition1, partition2];
+      },
+    },
+    findIndex: {
+      async value<T>(callback: (value: T) => boolean) {
+        for await (const [index, value] of this.asIndexedPairs()) {
+          if (callback(value))
+            return index;
+        }
+
+        return -1;
+      } 
+    },
+    max: {
+      value() {
+        return this.reduce((acc: number, val: number) => {
+          if (acc > val)
+            return acc;
+          return val;
+        });
+      },
+    },
+    min: {
+      value() {
+        return this.reduce((acc: number, val: number) => {
+          if (acc < val)
+            return acc;
+          return val;
+        });
+      },
+    },
+    cycle: {
+      async *value() {
+        const values = [];
+        
+        for await (const value of this) {
+          values.push(value);
+          yield value;
+        }
+
+        while (true) {
+          for (const value of values) {
+            yield value;
+          }
+        }
+      },
     },
   });
 
